3887a85c02493afdf9405c3434e5d0b120d2f414
Andrzej Chmielowski <akhmelovskyi@bluesoft.net.pl>
2015-04-20 14:36:16 +0200

    fix

--- pik/src/main/java/com/springapp/mvc/model/Model.java
+++ pik/src/main/java/com/springapp/mvc/model/Model.java
@@ -1,6 +1,5 @@
 package com.springapp.mvc.model;
 
-import com.springapp.mvc.model.printers.Printers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
